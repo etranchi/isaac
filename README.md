a5109ab5ee449f3ca96700eb6622cf9783db014e
etranchi <etranchi@student.42.fr>
2019-05-17 15:03:12 +0200

    edit

---
+++ README.md
@@ -0,0 +1,1 @@
+Little script to get every isaac's img
--- main.py
+++ main.py
@@ -1,33 +1,53 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from enum import Enum
 import imageio as ImageIO
 import sys
 import os
+import re
 
 url = "http://pop-life.com/foursouls/"
 
-def createFileImg(url_img):
+class CardType(Enum):
+    character = "CHARACTER CARD"
+    treasure = "TREASURE CARD"
+    loot = "LOOT CARD"
+    monster = "MONSTER CARD"
+    eternal = "STARTING ITEM"
+    @classmethod
+    def has_value(cls, value):
+        return any(value == item.value for item in cls)
+
+
+def createFileImg(url_img, cardType):
     img_alt = os.path.basename(url_img)
-    open(os.path.join(os.path.curdir, img_alt), 'wb')
-    print(img_alt)
-    return img_alt # for now..
+    email = re.compile('\w+')
+    result = email.findall(img_alt)
+    result[0] = ''.join([i for i in result[0] if not i.isdigit()])
+    if CardType.has_value(cardType):
+        path = CardType(cardType).name + "/" + result[0] + "." + result[len(result) - 1]
+        open(os.path.join(os.path.curdir, path), 'wb')
+        return path
+    else:
+        return ""
 
 def getData(driver):
     ret = []
-    elements = driver.find_elements(By.CLASS_NAME, 'team-img')
-    for elm in elements:
-        elm = elm.find_element(By.TAG_NAME, 'img')
-        url_img = elm.get_attribute('src')
-        if url_img != "":
+    allElements = driver.find_elements(By.CLASS_NAME, 'single-team')
+    for elm in allElements:
+        cardType = elm.find_element(By.CLASS_NAME, 'team-info').find_element(By.TAG_NAME, 'h2').get_attribute('innerHTML')
+        url_img = elm.find_element(By.CLASS_NAME, 'team-img').find_element(By.TAG_NAME, 'img').get_attribute('src')
+        name = createFileImg(url_img, cardType)
+        if url_img != "" and name != "":
             img = ImageIO.imread(url_img)
-            ImageIO.imwrite(createFileImg(url_img),img, "png")
+            ImageIO.imwrite(name,img, "png")
     return ret
 
 driver = webdriver.Chrome() 
 data = []
 
-for x in range(33, 44):    
+for x in range(0, 44):    
     driver.get(url + "index.php?page=" + str(x))
     data.append(getData(driver))
 
